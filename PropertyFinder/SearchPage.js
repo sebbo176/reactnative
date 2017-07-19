8aa88f1b02d7da3e0d3e5627ddb1e7b73c9f6518
Sebastian Bolling <sebastian.bolling@gmail.com>
2017-07-19 21:02:40 +0200

    started working on searchpage

---
+++ PropertyFinder/SearchPage.js
@@ -0,0 +1,34 @@
+'use strict';
+
+import React, {Component} from 'react'
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableHighlight,
+  ActivityIndicator,
+  Image
+} from 'react-native';
+
+var styles = StyleSheet.create({
+  description: {
+    marginBottom: 20,
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565'
+  },
+  container: {
+    padding: 30,
+    marginTop: 65,
+    alignItems: 'center'
+  }
+});
+
+class SeachPage extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+    )
+  }
+}
