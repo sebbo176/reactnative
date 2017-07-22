7fec8436ec39a8f86b53ed6f018484cdc5b1b15b
Sebastian Bolling <sebastian.bolling@gmail.com>
2017-07-22 12:31:40 +0200

    added more styles

--- PropertyFinder/SearchPage.js
+++ PropertyFinder/SearchPage.js
@@ -22,6 +22,44 @@ var styles = StyleSheet.create({
     padding: 30,
     marginTop: 65,
     alignItems: 'center'
+  },
+  flowRight: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'stretch'
+  },
+  flowRight: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'stretch'
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center'
+  },
+  button: {
+    height: 36,
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
+  searchInput: {
+    height: 36,
+    padding: 4,
+    marginRight: 5,
+    flex: 4,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#48BBEC',
+    borderRadius: 8,
+    color: '#48BBEC'
   }
 });
 
@@ -37,17 +75,17 @@ class SearchPage extends Component {
         </Text>
         <View style={styles.flowRight}>
           <TextInput
-            stype={styles.searchInput}
-            placeholder='Search via name or postcode' />
-            <TouchableHighlight style={styles.button}
+            style={styles.searchInput}
+            placeholder='Search via name or postcode'/>
+          <TouchableHighlight style={styles.button}
               underlayColor='#99d9f4'>
-                <Text style={styles.buttonText}>Go</Text>
-              </TouchableHighlight>
+            <Text style={styles.buttonText}>Go</Text>
+          </TouchableHighlight>
         </View>
         <TouchableHighlight style={styles.button}
-          underlayColor='#99d9f4'>
-            <Text style={styles.buttonText}>Location</Text>
-          </TouchableHighlight>
+            underlayColor='#99d9f4'>
+          <Text style={styles.buttonText}>Location</Text>
+        </TouchableHighlight>
       </View>
     );
   }
