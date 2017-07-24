2ae5afa30cf32b71a5707fcca5a9d0b02b947055
Sebastian Bolling <sebastian.bolling@gmail.com>
2017-07-24 12:50:52 +0200

    Added api search request

--- PropertyFinder/SearchPage.js
+++ PropertyFinder/SearchPage.js
@@ -63,13 +63,50 @@ var styles = StyleSheet.create({
   }
 });
 
+function urlForQueryAndPage(key, value, pageNumber) {
+  var data = {
+    country: 'uk',
+    pretty: '1',
+    encoding: 'json',
+    listing_type: 'buy',
+    action: 'search_listings',
+    page: 'pageNumber'
+  };
+  data[key] = value;
+
+  var querystring = Object.keys(data)
+  .map(key => key + '=' + encodeURIComponent(data[key]))
+  .join('&');
+
+  return 'http://api.nestoria.co.uk/api?' + querystring;
+};
+
 class SearchPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchString: 'london'
+      searchString: 'london',
+      isLoading: false,
+      message: ''
     };
   }
+    _executeQuery(query) {
+      console.log(query);
+      this.setState({ isLoading: true});
+      fetch(query)
+      .then(response => response.json())
+      .then(json => this._handleResponse(json.response))
+      .catch(error =>
+        this.setState({
+          isLoading : false,
+          message: 'Something bad happened ' + error
+        }));
+    }
+
+    onSearchPressed() {
+      var query = urlForQueryAndPage('place_name', this.state.searchString, 1);
+      this._executeQuery(query);
+    }
 
   onSearchTextChanged(event) {
     console.log('onSearchTextChanged');
@@ -77,7 +114,17 @@ class SearchPage extends Component {
     console.log(this.state.searchString);
   }
 
+  _handleResponse(response) {
+    this.setstate({ isLoading: false , message: ''});
+    if(response.application_response_code.substr(0,1) === '1') {
+      console.log('Properties found: ' + response.listings.length);
+    } else {
+      this.setState({ message: 'Location not recognized; please try again'});
+    }
+  }
+
   render() {
+    var spinner = this.state.isLoading ? ( <ActivityIndicator size='large' />) : (<View/>);
     console.log('SearchPage.render')
     return (
       <View style={styles.container}>
@@ -94,7 +141,8 @@ class SearchPage extends Component {
             onChange={this.onSearchTextChanged.bind(this)}
             placeholder='Search via name or postcode'/>
           <TouchableHighlight style={styles.button}
-              underlayColor='#99d9f4'>
+              underlayColor='#99d9f4'
+              onPress={this.onSearchPressed.bind(this)}>
             <Text style={styles.buttonText}>Go</Text>
           </TouchableHighlight>
         </View>
@@ -105,6 +153,8 @@ class SearchPage extends Component {
           </TouchableHighlight>
         </View>
         <Image source={require('./Resources/house.png')} style={styles.image} />
+        {spinner}
+        <Text style={styles.description}>{this.state.message}</Text>
       </View>
     );
   }
