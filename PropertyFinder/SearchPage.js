91aaea618a3af076583a4df95d6410eaa8dc35e6
Sebastian Bolling <sebastian.bolling@gmail.com>
2017-07-25 12:32:54 +0200

    got the new component to show the searchresults

--- PropertyFinder/SearchPage.js
+++ PropertyFinder/SearchPage.js
@@ -11,6 +11,8 @@ import {
   Image
 } from 'react-native';
 
+var SearchResults = require('./SearchResults');
+
 var styles = StyleSheet.create({
   description: {
     marginBottom: 20,
@@ -117,7 +119,12 @@ class SearchPage extends Component {
   _handleResponse(response) {
     this.setState({ isLoading: false , message: ''});
     if(response.application_response_code.substr(0,1) === '1') {
-      console.log('Properties found: ' + response.listings.length);
+      this.props.navigator.push({
+        title: 'Results',
+        component: SearchResults,
+        passProps: {listings: response.listings}
+      });
+      //console.log('Properties found: ' + response.listings.length);
     } else {
       this.setState({ message: 'Location not recognized; please try again'});
     }
